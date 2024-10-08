7015651fc0b305809e68de6c58c012c55e28ec39
Malte <malte.pavon.olesen@hotmail.com>
2024-10-08 10:58:33 +0200

    test

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,7 +1,10 @@
-import org.junit.jupiter.api.Test;
+
+
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Tests {
+public class Test1 {
 
     @Test
     public void testEquality() {
